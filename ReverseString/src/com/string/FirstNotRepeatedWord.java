27894fbc6e02ea7d9cb6b9e0b2f9629dcd80a0cc
nehadubey751 <nehadubey751>
2023-08-01 18:00:46 +0530

    'java'

---
+++ ReverseString/src/com/string/FirstNotRepeatedWord.java
@@ -0,0 +1,22 @@
+package com.string;
+
+public class FirstNotRepeatedWord {
+
+	public static void main(String[] args) {
+		String input="AABCDEBC";
+		for(int i=0;i<input.length();i++) {
+			boolean unique=true;
+			for(int j=0;j<input.length();j++) {
+				if(i!=j && input.charAt(i)==input.charAt(j)) {
+					 unique=false;
+					
+				}
+			}
+			if(unique) {
+				System.out.println(input.charAt(i));
+				break;
+			}
+		}
+	}
+
+}
